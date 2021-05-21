4ffd75c7a5d5a50ca15090ca89e0beabf0a51d15
ktlo <ktlo@handtruth.com>
2021-05-21 18:10:11 +0300

    Fix coverage

---
+++ modules/internals/src/commonTest/kotlin/com/handtruth/mc/internals/test/AssertTest.kt
@@ -0,0 +1,11 @@
+package com.handtruth.mc.internals.test
+
+import com.handtruth.mc.internals.assert
+import kotlin.test.Test
+
+class AssertTest {
+    @Test
+    fun assertTest() {
+        assert(true)
+    }
+}
